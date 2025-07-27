12f55e7f8f5c00323f289ef26ec9d3e89fd14911
kamit4014 <k.amit4014@gmail.com>
2025-07-27 18:08:18 +0530

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+// Create a new Main.java file in a folder (e.g., HelloWorld2)
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
+}
